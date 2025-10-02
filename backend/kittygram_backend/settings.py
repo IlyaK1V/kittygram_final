385cc395a8384824db716d4d18590bf44bb5737f
Ilya Kravchenko <ilyak475@gmail.com>
2025-10-02 11:42:17 +0300

    six settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_django_key')
 
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '51.250.29.209',
+                 'kittygram-learning.ru']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
